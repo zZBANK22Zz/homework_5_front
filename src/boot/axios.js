7a3884d1774c0077c1ce55c3ade2bce3b380f827
bank <ananda2546@gmail.com>
2023-11-18 00:15:05 +0700

    two

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -7,7 +7,7 @@ import axios from 'axios'
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const RESTAPI = "จhttps://book-book-ten.vercel.app/api"
+const RESTAPI = "https://book-book-ten.vercel.app/api"
 // const RESTAPI = "https://manage-account-api.vercel.app/api"
 const api = axios.create({
   baseURL: RESTAPI,
